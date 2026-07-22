06b25e0521a869ebd7ff04337bfc06abed7d305a
Sagheer <sagheerahmed123465@gmail.com>
2026-07-22 08:14:41 -0700

    Complete website update: bilingual support, PDF catalog, floating WhatsApp

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -16,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
